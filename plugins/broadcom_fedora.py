251c4243613915879e246281766061855b91443b
yusuf sourani <youssef.m.sourani@gmail.com>
2018-09-20 11:32:41 +0300

    Update

---
+++ plugins/broadcom_fedora.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  broadcom_fedora.py
+#  
+#  Copyright 2018 youcef sourani <youssef.m.sourani@gmail.com>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+from universalplugin.uplugin import BasePlugin, get_uniq_name, write_to_tmp
+import subprocess
+import time
+import os
+
+if_true_skip         = False
+if_false_skip        = True
+if_one_true_skip     = [False,False]
+if_all_true_skip     = [True,False]
+                
+arch                 = ["all"]
+distro_name          = ["fedora"]
+distro_version       = ["all"]
+category             = "<b>System</b>"
+category_icon_theme = "applications-system"
+
+def issecureboot():
+    out = subprocess.Popen("mokutil --sb-state &>/dev/null",shell=True,stdout=subprocess.PIPE).communicate()[0].decode("utf-8").strip()
+    if out == "SecureBoot enabled":
+        return True
+    else:
+        return False
+
+if os.path.isfile("/usr/bin/mokutil"):
+    secureboot = issecureboot()
+else:
+    secureboot = None
+
+ifinstallsucessmsg = "Install Broadcom Wifi Driver Sucess\n\n<span foreground=\"red\">Update Your System And Reboot</span>"
+if secureboot:
+    ifinstallsucessmsg = ifinstallsucessmsg+"\n\n<span foreground=\"red\">And Disable Secure Boot</span>"
+
+elif secureboot==None:
+    ifinstallsucessmsg = ifinstallsucessmsg+"\n\n<span foreground=\"red\">And Check Secure Boot If Disabled</span>"
+
+all_package = ["akmod-wl", "broadcom-wl"]
+
+class Plugin(BasePlugin):
+    __gtype_name__ = get_uniq_name(__file__) #uniq name and no space
+    def __init__(self,parent):
+        BasePlugin.__init__(self,parent=parent,
+                            spacing=2,
+                            margin=10,
+                            button_image="broadcom.jpg",
+                            button_install_label="Install Broadcom Wifi Driver",
+                            button_remove_label="Remove Broadcom Wifi Driver",
+                            buttontooltip="Install Remove Broadcom Wifi Driver",
+                            buttonsizewidth=100,
+                            buttonsizeheight=100,
+                            button_relief=2,
+                            blockparent=True,
+                            waitmsg="Wait...",
+                            runningmsg="Running...",
+                            loadingmsg="Loading...",
+                            ifinstallfailmsg="Install Broadcom Wifi Driver Failed",
+                            ifremovefailmsg="Remove Broadcom Wifi Driver Failed",
+                            ifinstallsucessmsg=ifinstallsucessmsg,
+                            expand=False)
+
+
+        
+        
+    def check(self):
+        check_package = all([self.check_package(pack) for pack in all_package])
+        return not check_package
+        
+    def install(self):
+        rpmfusion  = all([ self.check_package(pack) for pack in ["rpmfusion-nonfree-release", "rpmfusion-free-release"]])
+        to_install = [pack for pack in all_package if not self.check_package(pack)] + ["kernel", "kernel-devel", "kernel-headers", "@c-development"]
+        to_install = " ".join(to_install)
+        commands = ["dnf install {} -y --best".format(to_install)]
+        if not rpmfusion:
+            d_version = self.get_distro_version()
+            command_to_install_rpmfusion = "dnf install  --best -y --nogpgcheck  \
+    http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-{}.noarch.rpm \
+    http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-{}.noarch.rpm".format(d_version,d_version)
+            commands.insert(0,command_to_install_rpmfusion)
+        to_run = write_to_tmp(commands)
+
+        if subprocess.call("pkexec bash  {}".format(to_run),shell=True)==0:
+            return True
+        return False
+        
+    def remove(self):
+        to_remove = " ".join([pack for pack in all_package if self.check_package(pack)])
+        if subprocess.call("pkexec rpm -v --nodeps -e {}".format(to_remove),shell=True)==0:
+            return True
+        return False
+
+    def check_package(self,package_name):
+        if subprocess.call("rpm -q {} &>/dev/null".format(package_name),shell=True) == 0:
+            return True
+        return False
+        
+    def get_distro_version(self):
+        result=""
+        if not os.path.isfile("/etc/os-release"):
+            return None
+        with open("/etc/os-release") as myfile:
+            for l in myfile:
+                if l.startswith("VERSION_ID"):
+                    result=l.split("=",1)[1].strip()
+        return result.replace("\"","").replace("'","")
+
